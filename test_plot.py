6f9030a4d5cad1d79400dc6d985aca72e1de5cf9
Jon <jone.leung.2012@gmail.com>
2020-12-22 19:10:43 +0000

    more updates

---
+++ test_plot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print('Hello world')
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as ply
+
+
+# In[7]:
+
+
+ply.plot([1,2,3,4])
+ply.show()
+
+
+# In[ ]:
+
+
+
+
